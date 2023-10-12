e07f7117a9c0b6cdca621a15a2c3eb177065e27e
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2023-10-12 10:55:04 -0300

    Include the font Roboto and create the load wait

--- PassLockPro/src/app/(stack)/index.tsx
+++ PassLockPro/src/app/(stack)/index.tsx
@@ -1,8 +1,18 @@
 import { View } from 'react-native';
 import HeaderHighlight from '../components/headerHighlight';
 import { layoutStyles } from '../styles/layoutStyles';
+import { useFonts } from 'expo-font';
+import { Roboto_400Regular } from '@expo-google-fonts/roboto';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+      Roboto_400Regular
+    })
+  
+  if(!fontsLoaded){
+    return null;
+  }
+  
   return (
     <View style={layoutStyles.container}>
       <HeaderHighlight />
